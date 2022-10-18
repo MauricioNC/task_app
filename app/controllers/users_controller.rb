6bf478baa0ce762f24976f9b06460a372654888c
MauricioNc <mauricio_navarro0@ucol.mx>
2022-10-17 23:08:59 -0500

    User authentication It was created an User controller that contains all the logic to create a new user. Also, usign the bcryp gem, it was necessary to create a new sessions controller to validate the user login. The user model has the has_secure_password helper that help us to authenticate the user. Finally, the sessions new view contains the login form and the users new contains the create account form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  before_action :authorized, only: [:show, :update, :delete]
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User created successfully"
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :username, :password, :password_confirmation)
   end
 end
