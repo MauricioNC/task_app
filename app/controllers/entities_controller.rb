46113b9f691e53243f7e105b24f6e59f2ea2147a
MauricioNc <mauricio_navarro0@ucol.mx>
2022-10-18 12:48:41 -0500

    New entity Was implemented the logic to create a new entity.
     * Entities controller: Contains the main logic to store a new entity
       into the DB, if the entity was stored successfully, then send a flash
       message notice and redirect to root path, otherwise send a flash error
       message and redirect to new entity path.
     * Entity model: Contians a validations for name and status, the
       presence of this fields is required.
     * User model: Contains missing association with the entity model.
     * Index view for entities (root path): if an entity exists, then this
       view will show the first entity, otherwise display a message tha
       does not exist any entity and an option to create a new entity.
     * New view for entities: Contains the form to create a ne entity, this
       form contains a hidden field with value user_id, this is to sotre the
       user id and by this way make a relation btween the entity and the user.
     * _entity file: Will display the entity selected, this file is not
       completed yet.

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,10 +1,27 @@
 class EntitiesController < ApplicationController
   before_action :authorized
   def index
-    @entity = Entity.all
+    @entity = Entity.first
   end
 
   def new
     @entity = Entity.new
+  end
+
+  def create
+    @entity = Entity.new(entities_params)
+    if @entity.save
+      flash[:notice] = "Entity created successfully"
+      redirect_to root_path
+    else
+      flash[:error] = "It was not possible to create new entity"
+      redirect_to new_entity_path
+    end
+  end
+
+  private
+
+  def entities_params
+    params.require(:entity).permit(:name, :status, :user_id)
   end
 end
