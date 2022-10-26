d38addfeb122d6bf8bbf9c4fb8002c754f960c94
MauricioNc <mauricio_navarro0@ucol.mx>
2022-10-26 15:30:42 -0500

    Was changed the logic to search an entity

--- app/controllers/searcher_controller.rb
+++ app/controllers/searcher_controller.rb
@@ -1,11 +1,21 @@
 class SearcherController < ApplicationController
   def search
-    @search_result = Entity.where(user_id: params[:user_id]).find_by("name LIKE ?", "%#{params[:entity_name]}%")
-    if @search_result
-      redirect_to entity_path(@search_result.id)
-    else
+    @search_result = Entity.where("name LIKE ?", "%#{params[:entity_name]}%")
+
+    if @search_result.empty?
       flash[:error] = "Entity not found"
       redirect_to root_path
+      return
+    end
+
+    @search_result.each do |entity|
+      if entity.user_id != session[:user_id]
+        flash[:error] = "Entity not found"
+        redirect_to root_path
+        return
+      else
+        redirect_to entity_path(entity.id)
+      end
     end
   end
 end
