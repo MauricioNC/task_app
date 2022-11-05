4f4b6b5cb4eecb123d1c8a0d823f0bb375bf62a1
MauricioNC <mauricionc19@gmailc.om>
2022-11-04 23:20:43 -0600

    Applying changes for the funcionality to show entity and task options

--- app/javascript/controllers/_entity.js
+++ app/javascript/controllers/_entity.js
@@ -1,29 +1,31 @@
-window.onload = () => {
-  if(document.querySelector('.show_options'))
+export class Entity
+{
+  show_entity_options()
   {
-    const entity_show_options = document.querySelector('.show_options');
-    const entity_options = document.getElementById('options');
-    let entity_options_display;
-  
-    if(entity_options) { 
-      entity_options_display = window.getComputedStyle(entity_options); 
-      entity_show_options.addEventListener('click', () => {
-        entity_options_display.getPropertyValue('display') == 'none'
-          ? entity_options.style.setProperty('display', 'block')
-          : entity_options.style.setProperty('display', 'none');
-      });
-    }
-    
-  }
-  const redirect_delay = () => {
-    flash_notice = document.getElementById("flash-notice");
+  if(document.querySelector('.show_options'))
+    {
+      const entity_show_options = document.querySelector('.show_options');
+      const entity_options = document.getElementById('options');
+      let entity_options_display;
 
-    if(flash_notice) {
-      setTimeout(() => {
-        window.location = '/';
-      }, 3000);
+      if(entity_options) {
+        entity_options_display = window.getComputedStyle(entity_options);
+        entity_show_options.addEventListener('click', () => {
+          entity_options_display.getPropertyValue('display') == 'none'
+            ? entity_options.style.setProperty('display', 'block')
+            : entity_options.style.setProperty('display', 'none');
+        });
+      }
+
+    }
+    const redirect_delay = () => {
+      flash_notice = document.getElementById("flash-notice");
+
+      if(flash_notice) {
+        setTimeout(() => {
+          window.location = '/';
+        }, 3000);
+      }
     }
   }
-
-  redirect_delay();
 }
